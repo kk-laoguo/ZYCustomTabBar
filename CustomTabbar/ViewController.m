62bcd3b0d29bd3d12132f6d9ac90a0d8b8cd4959
guozengying <572249347@qq.com>
2017-07-26 14:03:26 +0800

    Initial Commit

---
+++ CustomTabbar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CustomTabbar
+//
+//  Created by 今代科技 on 2017/7/26.
+//  Copyright © 2017年 GZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CustomTabbar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomTabbar
+//
+//  Created by 今代科技 on 2017/7/26.
+//  Copyright © 2017年 GZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
